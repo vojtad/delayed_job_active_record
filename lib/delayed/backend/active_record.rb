a6dbbbb70ce9346e278697bdc508a37ce49d91d6
Vojta Drbohlav <vojta.d@gmail.com>
2022-04-25 01:19:00 +0200

    use FOR UPDATE SKIP LOCKED locking when reserving job in postgresql db
    https://www.postgresql.org/docs/12/sql-select.html#SQL-FOR-UPDATE-SHARE

--- lib/delayed/backend/active_record.rb
+++ lib/delayed/backend/active_record.rb
@@ -124,13 +124,13 @@ module Delayed
 
         def self.reserve_with_scope_using_optimized_postgres(ready_scope, worker, now)
           # Custom SQL required for PostgreSQL because postgres does not support UPDATE...LIMIT
-          # This locks the single record 'FOR UPDATE' in the subquery
-          # http://www.postgresql.org/docs/9.0/static/sql-select.html#SQL-FOR-UPDATE-SHARE
+          # This locks the single record 'FOR UPDATE SKIP LOCKED' in the subquery
+          # https://www.postgresql.org/docs/12/sql-select.html#SQL-FOR-UPDATE-SHARE
           # Note: active_record would attempt to generate UPDATE...LIMIT like
           # SQL for Postgres if we use a .limit() filter, but it would not
-          # use 'FOR UPDATE' and we would have many locking conflicts
+          # use 'FOR UPDATE SKIP LOCKED' and we would have many locking conflicts
           quoted_name = connection.quote_table_name(table_name)
-          subquery    = ready_scope.limit(1).lock(true).select("id").to_sql
+          subquery    = ready_scope.limit(1).lock('FOR UPDATE SKIP LOCKED').select("id").to_sql
           sql         = "UPDATE #{quoted_name} SET locked_at = ?, locked_by = ? WHERE id IN (#{subquery}) RETURNING *"
           reserved    = find_by_sql([sql, now, worker.name])
           reserved[0]
